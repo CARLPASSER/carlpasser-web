ca10aff9d6541e453f8e8a55b997339936857d57
Hiroaki Mishima <hiroakimishima@HiroakinoMacBook-Pro.local>
2026-05-12 20:23:29 +0900

    fix utf8 charset

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -14,7 +14,7 @@ exports.handler = async () => {
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
         "Cache-Control": "no-store",
       },
       body: JSON.stringify(data),
